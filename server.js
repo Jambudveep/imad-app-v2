3f919560b8e381d1633c59705541dfd59befe2a0
Jambudveep <jambudveep@gmail.com>
2017-02-17 12:36:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', function (req, res){
+app.get('/article-one', function (req, res) {
     res.send('Article One Sent');
 });
-app.get('/article-two', function (req, res){
+app.get('/article-two', function (req, res) {
     res.send('Article Two Sent');
 });
-app.get('/article-three', function (req, res){
+app.get('/article-three', function (req, res) {
     res.send('Article Three Sent');
 });
 
